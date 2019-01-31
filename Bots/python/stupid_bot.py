76f73caed9e38e38754d0de7281187bfcf585c01
Olimpiada Brasileira de Informatica <obi196@zero.LABS-IC>
2019-01-31 10:39:40 -0200

    Bot randomico

--- Bots/python/stupid_bot.py
+++ Bots/python/stupid_bot.py
@@ -1,16 +1,17 @@
 from bot_interface import *
 import math
-
-
+import random
 
 class StupidBot(BotBase):
-
     def process(self, gamestate):
-
-        return Action(1, 1, 1, 1)
-
-
-
-
+        thrust = random.uniform(-1, 1);
+        sideThrustFront = random.uniform(-1, 1);
+        sideThrustBack = random.uniform(-1, 1);
+        shoot = random.uniform(0, 3);
+        return Action(
+            thrust,
+            sideThrustFront,
+            sideThrustBack,
+            shoot)
 
 GameState(StupidBot()).connect()
